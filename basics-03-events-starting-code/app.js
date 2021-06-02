ffd48d363ea447b18748dc8e08f398054630d14b
yozdil <yamac.ozdil@outlook.com>
2021-06-02 08:41:13 -0400

    use param to add/remove 5

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -5,11 +5,11 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    add() {
-      this.counter = this.counter + 1;
+    add(num) {
+      this.counter = this.counter + num;
     },
-    remove() {
-      this.counter = this.counter - 1;
+    remove(num) {
+      this.counter = this.counter - num;
     },
   },
 });
