ab6a627942c5f7bc126f02516552ca74ff1d9f97
yozdil <yamac.ozdil@outlook.com>
2021-06-04 15:47:09 -0400

    use badgelist and userinfo as local components too

--- 04-components/04-diving-deeper/01-starting-setup/src/App.vue
+++ 04-components/04-diving-deeper/01-starting-setup/src/App.vue
@@ -2,20 +2,24 @@
   <div>
     <!-- <the-header></the-header>  ----- We can also use the component this way  -->
     <TheHeader />
-    <badge-list></badge-list>
-    <user-info
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
 import TheHeader from "./components/TheHeader.vue";
 
 export default {
   components: {
+    BadgeList,
+    UserInfo,
     TheHeader,
   },
   data() {
