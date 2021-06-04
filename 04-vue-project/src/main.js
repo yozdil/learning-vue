6f33fffb48a9d6e106aaa8a6d6425502605fb329
yozdil <yamac.ozdil@outlook.com>
2021-06-04 08:45:36 -0400

    component setup

--- 04-vue-project/src/main.js
+++ 04-vue-project/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 
-import App from "./App.vue";
+import App from "./App";
+import FriendContact from "./components/FriendContact";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.component("friend-contact", FriendContact);
+
+app.mount("#app");
