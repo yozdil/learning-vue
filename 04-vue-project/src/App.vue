1920abed7eae554fd5944eacbdb832c3e65eed8c
yozdil <yamac.ozdil@outlook.com>
2021-06-04 08:32:40 -0400

    initialize App

---
+++ 04-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+  <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234567890",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "07894567890",
+          email: "juju@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
