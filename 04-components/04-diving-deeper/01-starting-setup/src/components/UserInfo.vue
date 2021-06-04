02b3a6237aed176fd720b0f012877060dc6f0f0a
yozdil <yamac.ozdil@outlook.com>
2021-06-04 16:04:00 -0400

    implement slots

--- 04-components/04-diving-deeper/01-starting-setup/src/components/UserInfo.vue
+++ 04-components/04-diving-deeper/01-starting-setup/src/components/UserInfo.vue
@@ -1,27 +1,22 @@
 <template>
   <section>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </header>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['fullName', 'infoText', 'role'],
+  props: ["fullName", "infoText", "role"],
 };
 </script>
 
-<style>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
+<style scoped>
 
 section div {
   display: flex;
