275fa3a510a9e5fd040d590c669ef0517aca7fb1
yozdil <yamac.ozdil@outlook.com>
2021-06-04 09:45:13 -0400

    emit toggle favorite to parent

--- 04-vue-project/src/components/FriendContact.vue
+++ 04-vue-project/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite ? "(Favorite)" : "" }}</h2>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide Details" : "Show Details" }}
@@ -16,6 +16,10 @@
 export default {
   // props: ["name", "phoneNumber", "emailAdress", "isFavorite"], <--- Easiest way to write props
   props: {
+    id: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
@@ -40,8 +44,6 @@ export default {
   data() {
     return {
       detailsAreVisible: false,
-      //To counter data mutation, uni-directional data flow
-      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
@@ -49,7 +51,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorite() {
-      this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit("toggle-favorite", this.id);
     },
   },
 };
