005a19ff3bf26a4699a1915f2beb483b98798d76
yozdil <yamac.ozdil@outlook.com>
2021-06-08 07:16:44 -0400

    use mapActions and mapGetters

--- 05-vuex/src/components/ChangeCounter.vue
+++ 05-vuex/src/components/ChangeCounter.vue
@@ -1,13 +1,24 @@
 <template>
-  <button @click="addOne">Add 2</button>
+  <button @click="inc">Add 2</button>
+  <button @click="incr({ value: 11 })">Add 11</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   methods: {
-    addOne() {
-      this.$store.dispatch('increment');
-    }
+    // addOne() {
+    //   this.$store.dispatch('increment');
+    // }
+
+    // ...mapActions(['increment', 'increase'])
+
+    // OR
+
+    ...mapActions({
+      inc: 'increment',
+      incr: 'increase'
+    })
   }
 };
 </script>
--- 05-vuex/src/components/TheCounter.vue
+++ 05-vuex/src/components/TheCounter.vue
@@ -1,15 +1,18 @@
 <template>
   <h3>
-    {{ counter }}
+    {{ finalCounter }}
   </h3>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   computed: {
-    addOne() {
-      return this.$store.getters.finalCounter;
-    }
+    // addOne() {
+    //   return this.$store.getters.finalCounter;
+    // }
+    ...mapGetters(['finalCounter'])
   }
 };
 </script>
