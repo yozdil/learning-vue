a8aea166f897e847f65eb655c7485b5f4972ed2e
yozdil <yamac.ozdil@outlook.com>
2021-06-29 09:15:38 -0400

    add css

--- LearningResources/src/App.vue
+++ LearningResources/src/App.vue
@@ -37,4 +37,18 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
