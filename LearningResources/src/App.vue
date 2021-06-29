32c9f07740d5faa382175584655bf927ca102afc
yozdil <yamac.ozdil@outlook.com>
2021-06-29 09:13:56 -0400

    setup LearningResource

--- LearningResources/src/App.vue
+++ LearningResources/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <ul></ul>
+  <ul>
+    <LearningResource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></LearningResource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
 export default {
+  components: {
+    LearningResource
+  },
   data() {
     return {
       storedResources: [
