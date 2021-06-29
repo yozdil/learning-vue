78a0b3287ae66ef5e359d530e7ffa2de8a509099
yozdil <yamac.ozdil@outlook.com>
2021-06-29 09:35:53 -0400

    init Header & BaseCard

--- LearningResources/src/App.vue
+++ LearningResources/src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <ul>
-    <StoredResources :resources="storedResources"></StoredResources>
-  </ul>
+  <TheHeader title="Remember Me"></TheHeader>
+  <StoredResources :resources="storedResources"></StoredResources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 export default {
   components: {
-    StoredResources
+    StoredResources,
+    TheHeader
   },
   data() {
     return {
--- LearningResources/src/components/learning-resources/LearningResource.vue
+++ LearningResources/src/components/learning-resources/LearningResource.vue
@@ -1,20 +1,22 @@
 <template>
   <li>
-    <div>
+    <BaseCard>
       <header>
         <h3>{{ title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </BaseCard>
   </li>
 </template>
 
 <script>
+import BaseCard from '../ui/BaseCard.vue';
 export default {
+  components: { BaseCard },
   props: ['title', 'description', 'link']
 };
 </script>
